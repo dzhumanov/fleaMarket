f0e7350800458b06c2fcfb3ee4b5a78336b2fe3a
Temirlan <temadjumanov@gmail.com>
2024-03-02 13:29:17 +0600

    Configured project

---
+++ frontend/src/theme.ts
@@ -0,0 +1,17 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+  components: {
+    MuiTextField: {
+      defaultProps: {
+        variant: "outlined",
+        fullWidth: true,
+      },
+    },
+  },
+  // typography: {
+  //   fontFamily: ["SpaceGrotesk", "SpaceGroteskBold"].join(","),
+  // },
+});
+
+export default theme;
