ca71780658830252ba6c2df011acb75d50b3b45e
Temirlan <temadjumanov@gmail.com>
2024-03-02 13:36:53 +0600

    Created user model & router

---
+++ api/index.ts
@@ -0,0 +1,28 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import config from "./config";
+import userRouter from "./routers/users";
+
+const app = express();
+const port = 8000;
+
+app.use(express.static("public"));
+app.use(express.json());
+app.use(cors());
+
+app.use("/users", userRouter);
+
+const run = async () => {
+  await mongoose.connect(config.mongoose.db);
+
+  app.listen(port, () => {
+    console.log(`Server started on ${port} port!`);
+  });
+
+  process.on("exit", () => {
+    mongoose.disconnect();
+  });
+};
+
+void run();
